640c172bedd6f209170cbffd6481128c7ae7b696
ewush956 <ewush956@mtroyal.ca>
2024-11-13 15:10:56 -0700

    Add FCFS disk scheduling algorithm implementation

---
+++ algorithms.js
@@ -0,0 +1,45 @@
+class Request {
+    constructor(trackNumber) {
+        this.trackNumber = trackNumber;
+    }
+
+    getTrackNumber() {
+        return this.trackNumber;
+    }
+
+    toString() {
+        return String(this.trackNumber);
+    }
+}
+
+function runFCFS(queue, head) {
+    let currentLocation;
+    let seekCounter = 0;
+    let distance = 0;
+
+    for (let i = 0; i < queue.length; i++) {
+        currentLocation = queue[i].getTrackNumber();
+        distance = Math.abs(currentLocation - head);  // Corrected distance calculation
+        seekCounter += distance;
+        head = currentLocation;
+    }
+
+    console.log("Total Number of seek operations = " + seekCounter);
+    console.log("The Seek Sequence is");
+    for (let i = 0; i < queue.length; i++) {
+        console.log(queue[i].toString());
+    }
+}
+
+// Example usage
+/*
+const initialHeadPosition = 40; // Example initial head position
+const input = "40,20,67,89,8"; // Example disk request sequence
+
+const inputArray = input.split(',');
+const requests = inputArray.map(trackNumber => new Request(parseInt(trackNumber)));
+
+runFCFS(requests, initialHeadPosition);
+*/
+// Export the class and function if using modules
+export { Request, runFCFS };
