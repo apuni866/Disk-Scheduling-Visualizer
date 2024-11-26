ab48258c3a64121891d4902d43b8faa5dacd84fa
ewush956 <ewush956@mtroyal.ca>
2024-11-26 12:46:19 -0700

    Enable start simulation button upon algorithm selection and update button text dynamically

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import { Request, runFCFS } from './algorithms.js';
 document.addEventListener('DOMContentLoaded', () => {
     let selectedAlgorithm = null;
     const algorithms = ['fcfs', 'scan', 'cscan', 'look', 'clook', 'sstf'];
+    const startSimulationButton = document.querySelector('#start-simulation');
 
     algorithms.forEach(algorithm => {
         document.querySelector(`#${algorithm}`).addEventListener('click', () => {
@@ -11,6 +12,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.querySelector(`#${algo}`).classList.remove('glowing-border');
             });
             document.querySelector(`#${algorithm}`).classList.add('glowing-border');
+            startSimulationButton.disabled = false;
+            startSimulationButton.classList.remove('bg-gray-400', 'cursor-not-allowed');
+            startSimulationButton.classList.add('bg-blue-700', 'hover:bg-blue-800');
+            updateStartButton(selectedAlgorithm);
         });
     });
 
@@ -24,12 +29,36 @@ document.addEventListener('DOMContentLoaded', () => {
             alert('Please select an algorithm first.');
         }
     });
+
+    document.querySelector('#reset-form').addEventListener('click', () => {
+        document.querySelector('#simulation-form').reset();
+        selectedAlgorithm = null;
+        algorithms.forEach(algo => {
+            document.querySelector(`#${algo}`).classList.remove('glowing-border');
+        });
+        startSimulationButton.disabled = true;
+        startSimulationButton.classList.add('bg-gray-400', 'cursor-not-allowed');
+        startSimulationButton.classList.remove('bg-blue-700', 'hover:bg-blue-800');
+        //startSimulationButton.textContent = 'Start Simulation';
+    });
 });
+
+function updateStartButton(selectedAlgorithm) {
+    const startSimulationButton = document.querySelector('#start-simulation');
+    const algorithmNames = {
+        'fcfs': 'FCFS',
+        'scan': 'SCAN',
+        'cscan': 'C-SCAN',
+        'look': 'LOOK',
+        'clook': 'C-LOOK',
+        'sstf': 'SSTF'
+    };
+    startSimulationButton.textContent = `Run ${algorithmNames[selectedAlgorithm]} Simulation`;
+}
 
 function startSimulation(algorithm, initialHeadPosition, diskRequestSequence) {
     console.log(`Starting simulation with ${algorithm} algorithm`);
     const requests = diskRequestSequence.map(trackNumber => new Request(trackNumber));
-    // Change page view if valid algorithm is selected !!!
     if (algorithm === 'fcfs') {
         console.log('Starting FCFS simulation');
         runFCFS(requests, initialHeadPosition);
